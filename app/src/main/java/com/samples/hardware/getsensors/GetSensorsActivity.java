d842fb20f9b66f0823d7ec76bf2b8f311b589a17
java2019 <java_2019@mail.ru>
2017-07-04 15:52:50 +0300

    test1

--- app/src/main/java/com/samples/hardware/getsensors/GetSensorsActivity.java
+++ app/src/main/java/com/samples/hardware/getsensors/GetSensorsActivity.java
@@ -1,7 +1,12 @@
 package com.samples.hardware.getsensors;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class GetSensorsActivity extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class GetSensorsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_sensors);
+
+        TextView text = (TextView)findViewById(R.id.text);
+
+        SensorManager manager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        List<Sensor> sensorList = manager.getSensorList(Sensor.TYPE_ALL);
+        text.append("Available sensors:");
+        for (Sensor sensor:sensorList){
+            text.append("\n" + sensor.getName());
+        }
     }
 }
